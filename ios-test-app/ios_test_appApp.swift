d35a5606fea4b4c4bb71228e04cc855974435e89
surjo976 <surjosen975@gmail.com>
2025-05-07 12:42:00 +0600

    Initial Commit

---
+++ ios-test-app/ios_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_test_appApp.swift
+//  ios-test-app
+//
+//  Created by Appstick on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
